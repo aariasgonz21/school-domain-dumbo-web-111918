6178fc8ccc4237ee3e98d14a7e1382a436f52372
github username <github email address>
2018-11-24 22:59:15 -0500

    made school domain

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+require "pry"
+
+class School
+  attr_accessor :roster
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    name_arr = []
+    name_arr << name
+
+    if @roster.include?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = name_arr
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade|
+      #binding.pry
+      grade[1].sort!
+    end
+  end
+end
